164fdad9ac4391993eb1ba370d5fc2bb2defd2d0
Riley Hopper <Rileyhop1@gmail.com>
2026-08-26 10:49:46 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,21 @@
+#include <unordered_map>
+#include <algorithm>
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> sets{};
+        vector<vector<string>> output{};
+
+        for(const auto &s : strs) {
+            auto sorted = s;
+            sort(sorted.begin(), sorted.end());
+            sets[sorted].emplace_back(s);
+        }
+
+        for (const auto &s: sets) output.emplace_back(s.second);
+        return output;
+
+
+    }
+};
