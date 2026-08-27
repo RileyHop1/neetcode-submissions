bdb29a7470272c369f350b40c2f1142c12e2fda6
Riley Hopper <Rileyhop1@gmail.com>
2026-08-27 12:42:29 -0700

    Add: binary-search - submission-21

---
+++ Data Structures & Algorithms/binary-search/submission-21.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int front = 0;
+        int back = nums.size() - 1;
+        while(front <= back) {
+            int middle = (back + front)/2;
+            if(target == nums[middle]) return middle;
+            else if (target > nums[middle]) front = middle + 1;
+            else back = middle - 1;
+        }
+        return -1;      
+    }
+};
