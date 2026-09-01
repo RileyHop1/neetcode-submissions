1aa0c3cb95b682981fdce2c4d2bcafce4867a8f1
Riley Hopper <Rileyhop1@gmail.com>
2026-09-01 12:35:27 -0700

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int islands{0};
+
+
+        for (int i{0}; i < grid.size(); ++i) {
+            for(int j{0}; j < grid[i].size(); ++j) {
+                if (grid[i][j] == 'x') continue;
+                if (grid[i][j] == '1') {
+                    islands++;
+                    markAdjacent(grid, i, j);
+                } else grid[i][j] = 'x'; 
+            }
+        }
+        return islands;
+
+
+        
+    }
+    void markAdjacent(vector<vector<char>>& grid, int i, int j) {
+        grid[i][j] = 'x'; 
+        std::cout << i << "," << j << std::endl;
+
+        if(i + 1 < grid.size() && grid[i + 1][j] == '1') {
+            markAdjacent(grid, i + 1, j);
+        }
+        if(j + 1 < grid[i].size() && grid[i][j + 1] == '1') {
+            markAdjacent(grid, i, j + 1);
+        }
+        if(i - 1 >= 0 && grid[i - 1][j] == '1') {
+            markAdjacent(grid, i - 1, j);
+        }
+        if(j - 1 >= 0 && grid[i][j - 1] == '1') {
+            markAdjacent(grid, i, j - 1);
+        }
+
+    }
+    
+};
