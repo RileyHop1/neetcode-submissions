d4b5642d2dbc39931ac0e99cb9cc34bf28c86864
Riley Hopper <Rileyhop1@gmail.com>
2026-09-02 14:30:19 -0700

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int largest{0};
+
+
+        for (int i{0}; i < grid.size(); ++i) {
+            for(int j{0}; j < grid[i].size(); ++j) {
+                if (grid[i][j] == -1) continue;
+                if (grid[i][j] == 1) {
+                    int temp = markAdjacent(grid, i, j);
+                    if (temp > largest) largest = temp;
+                } else grid[i][j] = -1; 
+            }
+        }
+        return largest;
+
+
+        
+    }
+    int markAdjacent(vector<vector<int>>& grid, int i, int j) {
+        grid[i][j] = -1;
+
+        int right = 0;
+        int left = 0;
+        int up = 0;
+        int down = 0; 
+
+        if(i + 1 < grid.size() && grid[i + 1][j] == 1) {
+            down = markAdjacent(grid, i + 1, j);
+        }
+        if(j + 1 < grid[i].size() && grid[i][j + 1] == 1) {
+            right = markAdjacent(grid, i, j + 1);
+        }
+        if(i - 1 >= 0 && grid[i - 1][j] == 1) {
+            up = markAdjacent(grid, i - 1, j);
+        }
+        if(j - 1 >= 0 && grid[i][j - 1] == 1) {
+            left = markAdjacent(grid, i, j - 1);
+        }
+
+        return right + left + up + down + 1;
+
+    }
+};
