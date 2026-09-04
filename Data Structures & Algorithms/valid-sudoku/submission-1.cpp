560669273c199393c67dd5cf753b20592760ffd2
Riley Hopper <Rileyhop1@gmail.com>
2026-09-04 15:25:21 -0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,48 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        auto board_size = board.size();
+        unordered_set<char> row;
+        vector<unordered_set<char>> columns(board_size);//Default constructing all the columns, empty.
+        vector<unordered_set<char>> squares(3);
+
+        for(unsigned int i{0}; i < board_size; ++i) {
+            if (i%3 == 0) {
+                for (auto& s: squares) {
+                    s.clear();
+                }
+            }
+            for (unsigned int j{0}; j < board[i].size(); ++j) {
+                auto cha = board[i][j];
+
+                int brick = j/3;
+                cout << brick << " i = "<< i << " j = " << j << endl;
+
+                if (cha == '.') continue;
+
+                
+
+                auto row_size = row.size();
+                auto column_size = columns[j].size();
+                auto square_size = squares[brick].size();
+
+                row.insert(cha);
+                columns[j].insert(cha);
+                squares[brick].insert(cha);
+
+                if (
+                    row.size() == row_size || 
+                    columns[j].size() == column_size ||
+                    squares[brick].size() == square_size
+                ) return false;
+            }
+            row.clear();
+        }
+
+
+
+        return true; //Default
+        
+    }
+};
