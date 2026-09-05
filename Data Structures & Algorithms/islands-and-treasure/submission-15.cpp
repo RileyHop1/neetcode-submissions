905dbe7f2faf0a293a6be46c9055e120cb710452
Riley Hopper <Rileyhop1@gmail.com>
2026-09-05 07:54:55 -0700

    Add: islands-and-treasure - submission-15

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-15.cpp
@@ -0,0 +1,40 @@
+#include <list>
+
+class Solution {
+public:
+    
+    
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        list<vector<int>> q;
+        for (int i{0}; i < grid.size(); ++i) {
+            for(int j{0}; j < grid[i].size(); ++j) {
+                if (grid[i][j] == 0)  q.push_back({i, j});
+            }
+        }
+
+        while(!q.empty()) {
+            int i = q.front().front();
+            int j = q.front().back();
+            int dist = grid[i][j] + 1;
+            if (i - 1 >= 0 && dist < grid[i-1][j]) {
+                q.push_back({i-1, j});
+                grid[i-1][j] = dist;
+            }
+            if (i + 1 < grid.size() && dist < grid[i+1][j]) {
+                q.push_back({i+1, j});
+                grid[i+1][j] = dist;
+            }
+            if (j - 1 >= 0 && dist < grid[i][j-1]) {
+                q.push_back({i, j-1});
+                grid[i][j-1] = dist;
+            }
+            if (j + 1 < grid[i].size() && dist < grid[i][j+1]) {
+                q.push_back({i, j+1});
+                grid[i][j+1] = dist;
+            }
+            q.pop_front();
+
+        }
+        
+    }
+};
