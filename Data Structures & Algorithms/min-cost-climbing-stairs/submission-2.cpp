4ee2d8f15a4a7273f4c5fb2410804f42f3ef5615
Riley Hopper <Rileyhop1@gmail.com>
2026-09-06 13:37:14 -0700

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.cpp
@@ -0,0 +1,30 @@
+#include <unordered_map>
+
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        unordered_map<int, int> ptcp;
+        return dfs(cost, -1, ptcp);
+    }
+
+    int dfs(vector<int>& cost, int i, unordered_map<int,int>& ptcp) {
+        if (i == cost.size() -1 ) return cost[i];
+        if (ptcp.contains(i)) return ptcp[i];
+
+        int left = 0;
+        int right = 0;
+
+        if (i + 1 < cost.size()) 
+            left = dfs(cost, i + 1, ptcp);
+        if (i + 2 < cost.size()) 
+            right = dfs(cost, i + 2, ptcp);
+
+        int mi = min(left,right);
+
+        if (i == -1) return mi;
+
+        ptcp[i] =  mi + cost[i];
+        return ptcp[i];
+    }
+};
