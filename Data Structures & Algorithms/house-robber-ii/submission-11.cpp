c8e19f06e467073ba8f2ca16927c2ff7738d97c2
Riley Hopper <Rileyhop1@gmail.com>
2026-09-14 12:19:52 -0700

    Add: house-robber-ii - submission-11

---
+++ Data Structures & Algorithms/house-robber-ii/submission-11.cpp
@@ -0,0 +1,31 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        if (nums.size() == 1) return nums.back();
+        
+        unordered_map<int, int> m;
+
+        if (nums.empty()) return 0;
+        int first = max(dfs(m, 0, nums, nums.size()-1)
+            , dfs(m,1,nums, nums.size()-1));
+        m.clear();
+        int second = max(dfs(m, 1, nums, nums.size())
+            , dfs(m,2,nums, nums.size()));
+    
+
+        return max(first, second);
+    }
+
+    int dfs(unordered_map<int, int>& m, int i, vector<int>& nums, int size) {
+        if (i >= size) return 0;
+        if (m.contains(i)) return m[i];
+        m[i] = max(dfs(m, i + 2, nums, size)
+            , dfs(m, i + 3, nums, size)) + nums[i];
+
+
+        return m[i];
+    }
+};
